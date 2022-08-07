e92c5cb128dd1af4c9b67fa7c68cdf4c2347beb9
coollol <109149835+coollol@users.noreply.github.com>
2022-08-07 19:14:22 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    value += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(value + _2value)
+})
+input.onButtonPressed(Button.B, function () {
+    _2value += 1
+})
+let _2value = 0
+let value = 0
+value = 0
+_2value = 0
